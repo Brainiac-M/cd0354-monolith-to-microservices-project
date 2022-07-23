9c6a44d2675b2c8b25e3249536c657615b40c608
Fox_Coder508 <49205684+Brainiac-M@users.noreply.github.com>
2022-07-23 13:18:28 +0100

    initial commit

--- set_env.sh
+++ set_env.sh
@@ -1,10 +1,10 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_USERNAME=project3
+export POSTGRES_PASSWORD=project3
+export POSTGRES_HOST=project3-database.ckfse4xku6vf.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export AWS_BUCKET=mahmood-project3-bucket
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
