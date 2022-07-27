f9452ad73a2e9364480c134b96d2240178e66ad6
Fox_Coder508 <49205684+Brainiac-M@users.noreply.github.com>
2022-07-27 11:18:26 +0100

    fron-end apiHost updated

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://acde42d12cb0f4828ac080f0dd63669b-486129064.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a3424fbba28eb4cfba6ae158fadf1be7-1782013106.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
